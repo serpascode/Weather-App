8f8cdf07452e25363d29e34e59290b1fedbd1ee2
Luis Serpas <serpas.icode@gmail.com>
2020-03-17 18:11:25 -0400

    Update Weather Component to display returned Weather Data, Add Try / Catch for an incorrect location

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import Weather from './components/Weather';
 const API_KEY = "bfb8fcb033cac70076f86966f996c4f6";
 
 class App extends React.Component{
+  state = {
+      name: undefined,
+      country: undefined,
+      temperature: undefined,
+      weather: undefined,
+      error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const CITY = e.target.elements.city.value;
@@ -14,14 +22,34 @@ class App extends React.Component{
 
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${CITY},${COUNTRY}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
-    console.log(data);
+
+    if(CITY && COUNTRY){
+      try{
+      this.setState({
+        city: data.name,
+        country: data.sys.country,
+        weather: data.weather[0].description,
+        temperature: data.main.temp,
+        error: ""
+      });}catch(e){this.setState({error: "Enter a Valid City and Country!"});}
+    }else{
+      this.setState({
+        error: "Please Enter a City and Select Country"
+      });
+    }
   }
   render(){
     return(
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          weather={this.state.weather}
+          temperature={this.state.temperature}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,10 @@ class Weather extends React.Component{
   render(){
     return(
       <div>
-        Weather Details, Component
+        { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+        { this.props.weather  && <p>Weather: {this.props.weather}</p>}
+        { this.props.temperature && <p>Temperature: {this.props.temperature} Degrees</p>}
+        { this.props.error  && <p>Error: {this.props.error}</p>}
       </div>
     );
   }
